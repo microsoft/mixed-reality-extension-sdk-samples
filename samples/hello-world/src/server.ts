5fa7af97a63b5d0046689462717840191e7ec56e
Eric Anderson <eanders@microsoft.com>
2018-12-04 19:53:19 -0800

    Move WebHost out of application class (#5)
    * Move WebHost out of application class
    
    * disable debug spew

---
+++ samples/hello-world/src/server.ts
@@ -0,0 +1,21 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { ConsoleLogger, WebHost } from '@microsoft/mixed-reality-extension-sdk';
+import { resolve as resolvePath } from 'path';
+import HelloWorld from './app';
+
+const logger = new ConsoleLogger();
+// logger.disable('debug', 'success');
+
+ // Set up static file hosting, initialize adapter
+const server = new WebHost({
+    baseDir: resolvePath(__dirname, '../public'),
+    port: 3901,
+    logger
+});
+
+// When the webhost is set up, initialize the MRE system
+server.adapter.onConnection(context => new HelloWorld(context, server.baseUrl));
