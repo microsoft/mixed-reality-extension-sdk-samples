bee2e45e95470c802e06d6e889ca64df0c86ba3f
Eric Anderson <eanders@microsoft.com>
2018-12-10 14:49:07 -0800

    Update to latest SDK (#17)
    * don't lint on build
    
    * Update to match latest SDK
    
    * Use default baseDir value
    
    * Update SDK version

--- samples/hello-world/src/server.ts
+++ samples/hello-world/src/server.ts
@@ -3,21 +3,14 @@
  * Licensed under the MIT License.
  */
 
-import { ConsoleLogger, WebHost } from '@microsoft/mixed-reality-extension-sdk';
-import { resolve as resolvePath } from 'path';
+import { WebHost } from '@microsoft/mixed-reality-extension-sdk';
 import HelloWorld from './app';
 
-process.on('uncaughtException', (err) => console.log('uncaughtException', err));
-process.on('unhandledRejection', (reason) => console.log('unhandledRejection', reason));
+process.on('uncaughtException', err => console.log('uncaughtException', err));
+process.on('unhandledRejection', reason => console.log('unhandledRejection', reason));
 
-const logger = new ConsoleLogger();
-logger.disable('debug', 'success');
-
- // Start listening for connections, and serve static files
-const server = new WebHost({
-   baseDir: resolvePath(__dirname, '../public'),
-   logger
-});
+// Start listening for connections, and serve static files
+const server = new WebHost();
 
 // Handle new application sessions
 server.adapter.onConnection(context => new HelloWorld(context, server.baseUrl));
