c59c5e2b024128f8893d5a054e381bdae97669ad
Soren Hannibal Nielsen <soren@microsoft.com>
2018-12-31 15:02:09 -0800

    From a clean repository, duplicated hello-world folder and renamed it tic-tac-toe. Prerequisite: have successfully deployed and tested the Hello World MRE.

---
+++ samples/tic-tac-toe/src/server.ts
@@ -0,0 +1,19 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { WebHost } from '@microsoft/mixed-reality-extension-sdk';
+import { resolve as resolvePath } from 'path';
+import HelloWorld from './app';
+
+process.on('uncaughtException', err => console.log('uncaughtException', err));
+process.on('unhandledRejection', reason => console.log('unhandledRejection', reason));
+
+// Start listening for connections, and serve static files
+const server = new WebHost({
+    baseDir: resolvePath(__dirname, '../public')
+});
+
+// Handle new application sessions
+server.adapter.onConnection(context => new HelloWorld(context, server.baseUrl));
