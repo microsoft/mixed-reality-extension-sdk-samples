04b902110b1981d921a413daa733a01cbf433aed
Eric Anderson <eanders@microsoft.com>
2019-02-25 11:43:58 -0800

    Comma cleanup

--- samples/tic-tac-toe/src/server.ts
+++ samples/tic-tac-toe/src/server.ts
@@ -14,8 +14,8 @@ process.on('unhandledRejection', reason => console.log('unhandledRejection', rea
 
 // Start listening for connections, and serve static files
 const server = new WebHost({
-    baseDir: resolvePath(__dirname, '../public'),
-    // baseUrl: 'http://<ngrok-id>.ngrok.io'
+    // baseUrl: 'http://<ngrok-id>.ngrok.io',
+    baseDir: resolvePath(__dirname, '../public')
 });
 
 // Handle new application sessions
