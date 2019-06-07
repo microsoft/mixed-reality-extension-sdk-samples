2620818fd3be1d2ef56aab0ca0e1a3fba3894cf9
Adrian Balanon <adbalano@microsoft.com>
2019-06-07 16:55:54 -0700

    Add abalanon/prototype branch
    Set up a branch to start learning MRE development.

---
+++ samples/acb-prototype/src/server.ts
@@ -0,0 +1,26 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { log, WebHost } from '@microsoft/mixed-reality-extension-sdk';
+import dotenv from 'dotenv';
+import { resolve as resolvePath } from 'path';
+import App from './app';
+
+// Read .env if file exists
+dotenv.config();
+
+process.on('uncaughtException', err => console.log('uncaughtException', err));
+process.on('unhandledRejection', reason => console.log('unhandledRejection', reason));
+
+log.enable('app');
+
+// Start listening for connections, and serve static files
+const server = new WebHost({
+    // baseUrl: 'http://<ngrok-id>.ngrok.io',
+    baseDir: resolvePath(__dirname, '../public')
+});
+
+// Handle new application sessions
+server.adapter.onConnection(context => new App(context, server.baseUrl));
