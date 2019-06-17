ef3480f7e030224f0005e8c69fe9eff0d6e158cb
Adrian Balanon <adbalano@microsoft.com>
2019-06-16 23:00:45 -0700

    Added song duration tracker to allow autoplay through list.
    Radio can now loop through playlist and restart itself.

--- samples/acb-prototype/src/app.ts
+++ samples/acb-prototype/src/app.ts
@@ -12,18 +12,17 @@ import {
     AssetManager,
     ButtonBehavior,
     Context,
+    ForwardPromise,
     Quaternion,
+    Sound,
+    SoundInstance,
     TextAnchorLocation,
     Vector3,
-    Vector3Like,
-    ForwardPromise,
-    Sound,
-    SoundInstance
+    Vector3Like
 } from '@microsoft/mixed-reality-extension-sdk';
 
-//import delay from '../src/utils/delay';
-
 import { deflateSync } from 'zlib';
+import delay from './utils/delay';
 
 /**
  * The main class of this app. All the logic goes here.
@@ -37,6 +36,8 @@ export default class HelloWorld {
     private buttonPlayState = false;
     private trackIndex = 0;
     private trackSoundInstance: ForwardPromise<SoundInstance> = null;
+    private songDurationTracker: Promise<void> = null;
+    private tracks: Array<ForwardPromise<Sound>> = [];
 
     constructor(private context: Context, private baseUrl: string) {
         this.context.onStarted(() => this.started());
@@ -77,7 +78,7 @@ export default class HelloWorld {
                 // Parent the glTF model to the text actor.
                 transform: {
                     local: {
-                        position: { x: 0.2, y: -1, z: 0 }, 
+                        position: { x: 0.2, y: -1, z: 0 },
                         scale: this.buttonScale
                     }
                 }
@@ -107,58 +108,55 @@ export default class HelloWorld {
         this.cubeNext = buttonPromiseNext.value;
         this.cubeLast = buttonPromiseLast.value;
 
-        let buttons: Actor[] = [this.cubePlay, this.cubeNext, this.cubeLast];
+        const buttons: Actor[] = [this.cubePlay, this.cubeNext, this.cubeLast];
 
-        for( let i = 0; i < buttons.length; i++)
-        {
+        for ( let i = 0; i < buttons.length; i++) {
            buttons[i].createAnimation(
                 'ButtonPress', {
                     keyframes: this.generatePressKeyFrames( 1.0, buttons[i] ),
                     events: []
                 }
-            )
-        };
+            );
+        }
 
         const buttonBehaviorPlay = this.cubePlay.setBehavior(ButtonBehavior);
         const buttonBehaviorNext = this.cubeNext.setBehavior(ButtonBehavior);
         const buttonBehaviorLast = this.cubeLast.setBehavior(ButtonBehavior);
 
-        let buttonLogic: ButtonBehavior[] = [ buttonBehaviorPlay, buttonBehaviorNext, buttonBehaviorLast];
+        const buttonLogic: ButtonBehavior[] = [ buttonBehaviorPlay, buttonBehaviorNext, buttonBehaviorLast];
 
-        for( let i = 0; i < buttonLogic.length; i++)
-        {
+        for ( let i = 0; i < buttonLogic.length; i++) {
             buttonLogic[i].onHover('enter', () => {
                 buttons[i].animateTo(
-                    { transform: { local: { scale: { x: 0.11, y: 0.11, z: 0.11 } } } }, 0.3, AnimationEaseCurves.EaseOutSine);
-                    //LEARN: Need to ask if I can do expresions inside a property like scale.
+                    { transform: {
+                        local: {
+                            scale: { x: 0.11, y: 0.11, z: 0.11 } } } }, 0.3, AnimationEaseCurves.EaseOutSine);
+                    // LEARN: Need to ask if I can do expresions inside a property like scale.
             });
-                    
+
             buttonLogic[i].onHover('exit', () => {
                 buttons[i].animateTo(
                     { transform: { local: { scale: this.buttonScale } } }, 0.3, AnimationEaseCurves.EaseOutSine);
             });
-        };
+        }
 
         // Get reference for our track array
-        let tracks = this.createTracksArray(); 
-        let firstPlay: boolean = true;
-        
+        this.tracks = this.createTracksArray();
+        let firstPlay = true;
+
         // When clicked, push back.
         buttonBehaviorPlay.onClick('pressed', () => {
             this.cubePlay.enableAnimation('ButtonPress');
-            
+
             if (this.buttonPlayState === false && firstPlay ) {
-                //trackSoundInstance.value.resume();
-                this.playTrack( tracks[this.trackIndex]);
-                //this.monitorPlayingTrack( tracks[this.trackIndex] );
+                // trackSoundInstance.value.resume();
+                this.playTrack( this.tracks[this.trackIndex]);
                 this.buttonPlayState = true;
                 firstPlay = false;
-            }
-            else if ( this.buttonPlayState === false ) {
+            } else if ( this.buttonPlayState === false ) {
                 this.trackSoundInstance.value.resume();
                 this.buttonPlayState = true;
-            }
-            else if ( this.buttonPlayState === true ) {
+            } else if ( this.buttonPlayState === true ) {
                 this.trackSoundInstance.value.pause();
                 this.buttonPlayState = false;
             }
@@ -169,51 +167,63 @@ export default class HelloWorld {
         buttonBehaviorNext.onClick('pressed', () => {
             this.cubeNext.enableAnimation('ButtonPress');
             this.trackIndex++;
-            if (this.trackIndex >= tracks.length )
-            {
-                this.trackIndex = 0; //restart index
-            }    
+            if (this.trackIndex >= this.tracks.length ) {
+                this.trackIndex = 0; // restart index
+            }
             console.log(this.trackIndex);
 
             this.trackSoundInstance.value.stop();
-            this.playTrack( tracks[this.trackIndex]);
-
+            this.playTrack( this.tracks[this.trackIndex]);
         });
 
         buttonBehaviorLast.onClick('pressed', () => {
             this.cubeLast.enableAnimation('ButtonPress');
             this.trackIndex--;
-            if ( this.trackIndex < 0 )
-            {
-                this.trackIndex = (tracks.length - 1);
+            if ( this.trackIndex < 0 ) {
+                this.trackIndex = (this.tracks.length - 1);
             }
             console.log(this.trackIndex);
 
             this.trackSoundInstance.value.stop();
-            this.playTrack( tracks[this.trackIndex]);
+            this.playTrack( this.tracks[this.trackIndex]);
         });
 
     }
 
-    private playTrack(track: ForwardPromise<Sound>)
-    {
-        this.trackSoundInstance = null;
-        this.trackSoundInstance = this.cubePlay.startSound(track.value.id,
-            {volume: 1, looping: true, doppler: 0, spread: 0.7, }, 0.0 );
-    }
-    
-/*
-    private async monitorPlayingTrack( track: ForwardPromise<Sound> )
-    {
-        let trackDuration = track.value.duration;
-        console.log(trackDuration);
-        await delay( trackDuration* 1000 );
-        console.log("completed timer");
-    }
-*/
+    private playTrack( currenTrack: ForwardPromise<Sound>) {
+        if (this.songDurationTracker != null ) {
+            this.songDurationTracker = null;
+        }
 
-    private createTracksArray()
-    {
+        if ( this.trackSoundInstance != null ) {
+            this.trackSoundInstance.value.stop();
+            this.trackSoundInstance = null;
+        }
+
+        this.trackSoundInstance = this.cubePlay.startSound(currenTrack.value.id,
+            {volume: 1, looping: true, doppler: 0, spread: 0.7, }, 0.0 );
+
+        this.songDurationTracker = this.monitorPlayingTrack( this.tracks[this.trackIndex], this.tracks );
+    }
+
+    private async monitorPlayingTrack( track: ForwardPromise<Sound>, trackList: Array<ForwardPromise<Sound>> ) {
+        const trackDuration = track.value.duration;
+        console.log(trackDuration);
+        await delay( trackDuration * 1000 );
+        console.log("completed timer");
+
+        // song completed iterate to next one
+        this.trackIndex++;
+
+        // Make sure autoplay restarts list
+        if (this.trackIndex >= this.tracks.length ) {
+            this.trackIndex = 0; // restart index
+        }
+
+        this.playTrack( trackList[this.trackIndex]);
+    }
+
+    private createTracksArray() {
         // create audio
         const trackAssetPromise0 = this.context.assetManager.createSound(
             'Track1',
@@ -224,7 +234,7 @@ export default class HelloWorld {
             'Track2',
             { uri: `${this.baseUrl}/46-12-05 Cypress Canyon.ogg` }
         );
-        
+
         const trackAssetPromise2 = this.context.assetManager.createSound(
             'Track3',
             { uri: `${this.baseUrl}/48-08-09 Fourble.ogg` }
@@ -234,16 +244,16 @@ export default class HelloWorld {
             'Track4',
             { uri: `${this.baseUrl}/500317_Escape_Three_Skeleton_Key.ogg` }
         );
-        
-        let tracklist: ForwardPromise<Sound> [] = [ trackAssetPromise0, trackAssetPromise1, trackAssetPromise2, trackAssetPromise3 ];
+
+        const tracklist: Array<ForwardPromise<Sound>> = [ trackAssetPromise0,
+            trackAssetPromise1, trackAssetPromise2, trackAssetPromise3 ];
 
         console.log("tracklist size" + tracklist.length);
         return(tracklist);
     }
 
-    private generatePressKeyFrames(duration: number, button: Actor): AnimationKeyframe[] 
-    {
-        let buttonPos = <Vector3Like>button.transform.local.position;
+    private generatePressKeyFrames(duration: number, button: Actor): AnimationKeyframe[] {
+        const buttonPos =  button.transform.local.position as Vector3Like;
         return [{
             time: 0 * duration,
             value: { transform: { local: { position: buttonPos } } }
